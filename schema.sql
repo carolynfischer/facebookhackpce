3421cb55ffbc4559f09fd307d18ecb3c68fdc523
promila3 <promila3@gmail.com>
2016-05-14 16:50:45 -0700

    Scoring Sentiment

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ drop table if exists entries;
 create table entries(
   id integer primary key autoincrement,
   user text not null,
-  score integer not null
+  score integer not null,
+  last_post time not null
 );
 
 drop table if exists alerts;
@@ -10,5 +11,5 @@ create table alerts(
   id integer primary key autoincrement,
    text not null,
    sent_to text not null,
-   time text not null
+   last_alert time not null
 );
---
+++ score.py
@@ -0,0 +1,113 @@
+import json
+from pprint import pprint
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+
+@app.route('/get_posts', methods=['GET', 'POST'])
+def get_posts():
+	""" Takes in JSON data of posts made to Middle School page & parses it. JSON format is a list of dicts, with each dict being 1 post."""
+	a = "Hi filler"
+
+	return True
+    
+def parse():
+    # Keywords selected from: http://slate.me/KlDTQn
+    
+	keyword_weights = {
+						'fucking': 1,
+						'fucked':  1,
+						'done with':  1,
+						'drunk':  1,
+						'shite':  1,
+						'annoyed':  1,
+						'freaking':  1,
+						'pissed':  1,
+						'crap':  1,
+						'sucks':  1,
+						'shit': 1,
+
+						'bullshit':4,
+						'stressed': 4,
+						'hate': 4,
+						'stupid': 4,
+						'frustrated':4, 
+						'crappy': 4,
+						'miserable': 4,
+						'horrible': 4,
+						'disappointed':4, 
+						'fed up': 4,
+						'sick of': 4,
+						'for once':4,
+
+						'leave me alone':7,
+						'scared':  7,
+						'unwanted': 7,
+						'feeling sad': 7,
+						'feeling depressed':7, 
+						'helpless': 7,
+						'crying': 7,
+						'empty': 7,
+
+						'kill': 10,
+						'suicide':  10,
+						'depressed':  10,
+						'lonely':  10,
+						'depression':  10,
+						'dead':  10,
+						'better off dead': 10, 
+						'no one would miss me': 10, 
+						'gun':  10,
+						'jump off a bridge': 10 
+	}
+
+	keywords = set(keyword_weights.keys())
+
+    # The Facebook API does not currently return data on "How I'm Feeling."
+    # If this is available in the future, the script would search that data also.
+
+    # data = json file POSTed from API call
+    
+	msg_words = "Hi gun frustrated gun what"
+	#msg_words = data['message'].split(" ")
+	msg_words = msg_words.split(" ")
+	# msg_words : is a list of the words in a single post	
+    
+	# msg : data structure to hold the set of keywords corresponding to one post
+	# scores : scores
+	msg = set(msg_words)
+	score = 0
+	for word in msg: 
+		if word in keywords:
+    		# increment user's score : Word count for this keyword * weight for this keyword
+			score += msg_words.count(word) * keyword_weights[word]
+
+	print score
+  	# {
+	   # "data": [
+	   #   {
+	   #     "id": "546349135390552_1138601292831997", 
+	   #     "message": "test", 
+	   #     "from": {
+	   #       "category": "Restaurant/cafe", 
+	   #       "category_list": [
+	   #         {
+	   #           "id": "128673187201735", 
+	   #           "name": "Coffee Shop"
+	   #         }
+	   #       ], 
+	   #       "name": "Mahesh's Cafe", 
+	   #       "id": "546349135390552"
+	   #     }, 
+	   #     "created_time": "2015-05-19T22:51:20+0000"
+	   #   }, 
+
+
+if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=8080)
+	parse()

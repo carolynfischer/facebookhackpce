79a530246a8b0c9f82e1689f0354327b6b89978f
PCE Hackathon <facebookhackpce@gmail.com>
2016-05-14 23:55:44 +0000

    send a sms

--- cron.py
+++ cron.py
@@ -3,8 +3,12 @@ import app
 import json
 import datetime
 import dateutil.parser
+from twilio.rest import TwilioRestClient
 
 ACCESS_TOKEN = '***REMOVED***'
+account_sid = "***REMOVED***"
+auth_token  = "***REMOVED***"
+client = TwilioRestClient(account_sid, auth_token)
 
 def pull_posts():
     payload = {'method': 'get',
@@ -22,6 +26,10 @@ def pull_posts():
             message = post['message']
             time = dateutil.parser.parse(post['created_time'])
             print name, user_id, message, time
+            
+            if 'class' in message:
+                send_sms(name, message)
+                print "SENT A NOTIFICATION"
         
 def send_message():
     url = 'https://graph.facebook.com/v2.6/me/messages'
@@ -35,6 +43,15 @@ def send_message():
     r = requests.post('https://graph.facebook.com/v2.6/hackathonschool/feed', params=payload)
     print r.text
     
+def send_sms(name, message):
+    sms_input = "Please pay attention to and contact {}, they might be in need! \
+    They just posted the following comment and it's concerning: {}.".format(name, message)
+    message = client.messages.create(body=sms_input,
+        to="***REMOVED***",    # Replace with your phone number
+        from_="***REMOVED***") # Replace with your Twilio number
+    print message
+
 if __name__ == '__main__':
-    #pull_posts()
-    send_message()
+    pull_posts()
+    #send_message()
+    #send_sms()
